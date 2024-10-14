ebd87c3519eb5a98b3decbcc83516e301bf31e00
YouGuessedMyName <ivo@melse.info>
2024-10-14 15:06:48 +0200

    Create room

--- test_matrix.py
+++ test_matrix.py
@@ -1,6 +1,9 @@
 import requests
 import logging
 import copy
+import random
+import string
+from time import sleep
 
 ### ESTABLISH SESSIONS ###
 BASE_URL = "http://localhost:8008"
@@ -20,28 +23,62 @@ GET_TOKEN_BODY = {
 # Check if login with password is enabled
 login_enabled_response = requests.get(FULL_URL + "login")
 logging.info(login_enabled_response)
-assert login_enabled_response.ok, "Server disabled password login"
+assert login_enabled_response.ok, "Server disabled password login, or the server is not running at all."
 
 def generate_login_body(user, password) -> dict:
-    body_copy = copy.deepcopy(GET_TOKEN_BODY)
-    body_copy["identifier"]["user"] = user
-    body_copy["password"] = password
-    return body_copy
+  body_copy = copy.deepcopy(GET_TOKEN_BODY)
+  body_copy["identifier"]["user"] = user
+  body_copy["password"] = password
+  return body_copy
 
 def login_user(user, password) -> dict:
-    response = requests.post(
-        FULL_URL + "login",
-        json = generate_login_body(user, password)
-    )
-    logging.debug(response)
-    assert response.ok, f"Login failed for user {user}"
-    return response.json()
+  response = requests.post(
+      FULL_URL + "login",
+      json = generate_login_body(user, password)
+  )
+  logging.debug(response)
+  assert response.ok, f"Login failed for user {user}"
+  return response.json()
+  
+def random_room_name():
+  return ''.join(random.choices(string.ascii_uppercase + string.digits, k=50))
+
+
+CREATE_ROOM_JSON = {
+    "name":"room11",
+    "visibility":"public",
+    "preset":"public_chat",
+    "room_alias_name":"room11",
+    "topic":"TOPIC",
+    "initial_state":[]
+  }
+
+def create_room_json(visibility):
+  res = copy.deepcopy(CREATE_ROOM_JSON)
+  res["name"] = random_room_name()
+  res["room_alias_name"] = res["name"]
+  res["visibility"] = visibility
+  return res
 
 # Login to the three sessions.
 one_session = login_user("one", "one")
+sleep(0.2)
 two_session = login_user("two", "two")
+sleep(0.2)
 three_session = login_user("three", "three")
 print(one_session)
 
 ### START TESTS ###
+def test1():
+  """Creating a public room."""
+  create_room_json1 = create_room_json("public")
+  headers = {"Authorization": f"Bearer {one_session["access_token"]}"}
+  response_create_room_1 = requests.post(
+    FULL_URL + "createRoom",
+    headers=headers,
+    json= create_room_json1,
+  )
+  assert response_create_room_1.ok, "Failed to create room."
 
+
+test1()
